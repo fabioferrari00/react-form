0ffd9963c41ab590449d0d1ee84212f0843c2b05
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-01 13:02:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const initialArticles = [
+  {
+    id: 1,
+    title: 'Pasta'
+  },
+  {
+    id: 2,
+    title: 'Pane'
+  },
+  {
+    id: 3,
+    title: 'Uova'
+  },
+  {
+    id: 4,
+    title: 'Insalata'
+  },
+  {
+    id: 5,
+    title: 'Cipolla'
+  },
+  {
+    id: 6,
+    title: 'Frutta'
+  }];
+
+function App() {
+
+}
+
+export default App
