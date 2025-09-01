d724abf53745b190dd3b9daf2334db6b5a9e2ad3
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-01 14:27:51 +0200

    [feat] milestone 1 + start milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-const initialArticles = [
-  {
-    id: 1,
-    title: 'Pasta'
-  },
-  {
-    id: 2,
-    title: 'Pane'
-  },
-  {
-    id: 3,
-    title: 'Uova'
-  },
-  {
-    id: 4,
-    title: 'Insalata'
-  },
-  {
-    id: 5,
-    title: 'Cipolla'
-  },
-  {
-    id: 6,
-    title: 'Frutta'
-  }];
+import { useState } from "react";
+
+const initialArticles = ["Pasta", "Uova", "Pane", "Latte", "Merenda"];
 
 function App() {
-
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Lista prodotti</h1>
+          </div>
+          <div className="col-12">
+            <ul className="list-unstayled list-group">
+              {initialArticles.map((article, index) => {
+                return (
+                  <li className="list-group-item" key={index}>{article}</li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className="col-12">
+            <form>
+              <div className="d-flex">
+                <input type="text" className="form-control me-2" placeholder="Inserisci articolo" />
+                <button className="btn btn-success">Inserisci</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
